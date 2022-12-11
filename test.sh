1433c90c17472852091fda7cb8217b893bc6c7f8
euclaise <jpritsk@purdue.edu>
2022-12-11 16:23:55 -0500

    add test for reading

--- test.sh
+++ test.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
+set -e
+
+runtest() {
+    if [ "$test" = "$res" ]; then
+        echo PASS
+    else
+        echo FAIL
+    fi
+}
+
+cd test
 
 echo === FIB2 TEST ===
-test=$(cat fibout)
-res=$(go run . fib2.axe)
-if [ "$test" = "$res" ]; then
-    echo PASS
-else
-    echo FAIL
-fi
+test=$(cat "fib.out")
+res=$(go run .. "fib2.axe")
+runtest
+
+echo === READ TEST ===
+test=$(cat "fib.axe")
+res=$(go run .. "read.axe")
+runtest
